4df96a2999ace671fb89d09d800289a551f1b94e
penguyen <peternguyen008@gmail.com>
2016-02-07 12:58:19 -0500

    click_click_BOOM game complete2

--- js/click_click_BOOM.js
+++ js/click_click_BOOM.js
@@ -12,69 +12,6 @@ function Box(){
 function Board(){
   this.board;               // holds array of Box objects
   this.level;               // stores game board dimension (easy:5 med:10 hard:15)
-<<<<<<< HEAD
-  var $board = $('#board');
-
-  // START A NEW GAME
-  this.startGame = function(level){   
-    this.board = [];                            // empties game board
-    this.level = level;                         // stores game dimension value
-    var mines = (Math.pow(level,2))/5;          // 20% of total area of boxes
-    var size = ((30/level)-.125)+"em";
-
-    for(var i=0; i<Math.pow(level,2); i++){
-      var box = new Box();                      // creates a new Box object
-      this.board.push(box);                     // adds new Box object to board array
-      
-      var $cell = $('<div>');                   // creates a new <div> tag
-      $cell.addClass('box').attr('id', i+1).css({"width":size,"height":size});    // <div class="boxes" id="i+1">
-      $cell.on('click',function(event){
-        $boxNum = parseInt($(event.target).attr('id'));
-        console.log('You clicked Box '+$boxNum + '. It is a: ' +typeof($boxNum));
-        this.checkBox($boxNum,0);           // ERRRRORRRRRR 
-        //console.log(this);
-      });
-      $board.append($cell);                     // adds to HTML $board 
-    }
-    
-    for(var a=0; a<mines; a++){             // randomly places mines
-      do{                                   // ensures no repetitive random numbers
-        var random = Math.floor(Math.random()*Math.pow(level,2)); 
-      } while (this.board[random].mine);
-      this.board[random].isMine();          // Box object is now a mine
-    }
-  };
-
-  // checks & displays current box and its neighboring boxes ( RECURSION )
-  this.checkBox = function(box, depth){           // box is called from clicked <div> id (dont forget to -1!)
-    var numOfMines, neighbors;
-    //console.log('Checking box '+box+' at depth '+depth);        // testing
-
-    if(depth === 0){
-      this.board[box-1].show();                    
-      // is this a mine ??
-      if(this.board[box-1].mine === true){        // ITS A MINE! - call this.gameOver()
-        this.gameOver();                          // GAME OVER YOU LOSE !!! 
-      } else {                                   
-        numOfMines = countMines(box);             // gets # of mines around box(input)                                                // how do i call this?
-        neighbors = checkAround(box);             // gets array of neighboring boxes                                                       // how do i call this?
-        $current.text(numOfMines);                // adds to <div> to be seen on webpage
-        for(var i=0; i<neighbors.length; i++){
-          checkBox(neighbors[i], 1);              // calls itself with new box value (RECURSION!!)
-        }   
-      }         
-    } 
-    if(depth === 1){                              
-      if(this.board[box-1].mine === true){          // is this a mine ??
-        this.board[box-1].check = true;
-      } else {
-        numOfMines = countMines(box);             // gets # of mines around box(input)
-        this.board[box-1].show();
-        $current.text(numOfMines);                // adds to <div> to be seen on webpage
-      } 
-      return;
-    }
-=======
   this.player;              // determines 1 or 2 players
   var that = this;          // stores the 'this' into context!!
 
@@ -142,7 +79,6 @@ function Board(){
       this.player = (this.player === '' ? '' : (this.player === 'Player A' ? 'Player B' : 'Player A'));
       $('#info').text(this.player); 
     }   return;
->>>>>>> development
   };
 
   // returns array of neighboring boxes of the input
@@ -152,25 +88,6 @@ function Board(){
     var max = Math.pow(this.level,2); 
 
     if(box%this.level === 0){             // box is on right edge of game board
-<<<<<<< HEAD
-      neighborBox = [box-this.level-1, box-this.level, box-1, box+this.level-1, box+this.level];
-    } else if (box%this.level === 1){     // box is on left edge of game board
-      neighborBox = [box-this.level, box-this.level+1, box+1, box+this.level, box+this.level+1];
-    } else {                              // box is somewhere inside the game board
-      neighborBox = [box-this.level-1, box-this.level, box-this.level+1, box-1, box+1, box+this.level-1, box+this.level, box+this.level+1];
-    }
-
-    // original code REMIX
-    for(var i=1; i<=neighborBox.length; i++){                         // i represents each box # starting at 1
-      var current = neighborBox[i-1];                                 // neighborBox[0]
-      if(current>0 && current<=max && !this.board[current].check){    // ONLY IF num is between 0-max && check===false
-        neighbors.push(current);
-      }
-    } 
-
-    console.log('Neighbors of box '+box+': '+neighbors);
-
-=======
       neighborBox = [box-this.level-1, box-this.level, box-1, parseInt(box)+parseInt(this.level)-1, parseInt(box)+parseInt(this.level)];
     } else if (box%this.level === 1){     // box is on left edge of game board
       neighborBox = [box-this.level, box-this.level+1, box+1, parseInt(box)+parseInt(this.level), parseInt(box)+parseInt(this.level)+1];
@@ -185,37 +102,11 @@ function Board(){
         neighbors.push(current);  
       }
     } 
->>>>>>> development
     return neighbors;
   };
 
   // returns total num of mines around box(input)
   this.countMines = function(box){      
-<<<<<<< HEAD
-    var total_mines = 0;                                      // total number of mines
-    var neighbors = this.checkAround(box);  
-
-    for(var i=1; i<=neighbors.length; i++){
-      var current = neighbors[i-1];                           // neighbors[0]
-      if(this.board[(current-1)].mine){ total_mines+= 1; }    // this box is a mine
-    } 
-
-    console.log('There\s '+total_mines+' mines at box '+box);
-
-    return total_mines;
-  };
-
-  // check if all boxes are shown (default hidden: false)
-  this.checkWin = function(){                           
-    var notMines = [];
-    for(var i=0; i<this.board.length; i++){
-      if(this.board[i].mine === false){ notMines.push(this.board[i]); }      // array of NON-MINES    -- DRY this
-    }
-
-    for(var x=0; x<notMines.length; x++){
-      if(notMines[x].hidden === true){ return false; }        // a box is STILL hidden                -- DRY this
-    } return true;                                            // all boxes are shown - YOU WIN!
-=======
     var total_mines = 0;                      // total number of mines
     var neighbors = this.checkAround(box);    // get neighboring boxes
 
@@ -237,14 +128,10 @@ function Board(){
         win = false;      // LOSE: if any box is STILL hidden
       }                 
     } return win;         // WIN: ALL non-mines are not hidden
->>>>>>> development
   };
 
   // displays Game Over message
   this.gameOver = function(){
-<<<<<<< HEAD
-    alert(this.checkWin() ? 'Great Game! You Win!' : 'GAME OVER! YOU LOSE!'); 
-=======
     var $gameOver = $('<div>').attr('id', 'gg');
     $('#board').hide();                     // clears all boxes on game board 
     $('.help').hide();
@@ -255,35 +142,10 @@ function Board(){
     
     $gameOver.text(result).fadeIn(3000);
     $('#top').after($gameOver);
->>>>>>> development
   };      
 };
 
 $(document).ready(function(){
-<<<<<<< HEAD
-    console.log('lock and loaded');
-    var $cell;
-    var $level, $players, $boxNum, $box;              // $level=game-level $players=2players 
-                                                      // $boxNum=clicked div id#
-    $('.game-level').click(function(event){
-      $level = $(event.target).attr('id');      // grabs id of easy|medium|hard
-      console.log('this game will be: ' + $level);
-      
-      $('#levels').remove();                  // removes buttons
-      $('#instructions').remove();            // removes instructions
-      $('.container').append($('<div id="board">'));     // add new gameBoard to HTML
-      var ccb = new Board();
-      ccb.startGame($level);
-
-
-      console.log('Here\'s all the div boxes: '+ccb.board.length);
-      // do{
-      //   ccb.checkBox(,0);
-      // } while(!ccb.checkWin());
-      // ccb.gameOver();
-    });  
-  });
-=======
     var $level, $players, $boxNum;              // $level=game-level $boxNum=clicked div id#
     $('#instructions').hide();                  // hides instructions
 
@@ -312,8 +174,3 @@ $(document).ready(function(){
     });   // game-level 
 
 }); // document.ready
-
-
-
->>>>>>> development
-
